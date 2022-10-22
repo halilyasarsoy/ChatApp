1f90b1f6c1baca37539a74b85281516e2d53d5b3
Halil Yasarsoy <71354610+halilyasarsoy@users.noreply.github.com>
2022-10-22 22:20:47 +0300

    Tablayout added and Ui changed

--- app/src/main/java/com/halil/chatapp/adapter/ListAdapter.kt
+++ app/src/main/java/com/halil/chatapp/adapter/ListAdapter.kt
@@ -1,14 +1,8 @@
 package com.halil.chatapp.adapter
 
-import android.app.LauncherActivity.ListItem
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.gms.tasks.Task
-import com.halil.chatapp.R
-import com.halil.chatapp.data.User
 import com.halil.chatapp.data.Users
 import com.halil.chatapp.databinding.ListItemBinding
 
@@ -23,13 +17,10 @@ class ListAdapter(var userList : ArrayList<Users>) : RecyclerView.Adapter<ListAd
              itemBinding.tvFirstName.text = users.name
              itemBinding.tvLastName.text = users.lastname
          }
-//    val firstName : TextView = itemView.findViewById(R.id.tvFirstName)
-//    val lastName : TextView = itemView.findViewById(R.id.tvLastName)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
      return MyViewHolder(ListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
-
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
--- app/src/main/java/com/halil/chatapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/halil/chatapp/ui/activity/MainActivity.kt
@@ -1,47 +1,44 @@
 package com.halil.chatapp.ui.activity
 
 import android.content.Intent
+import android.content.res.Resources.NotFoundException
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.GridLayout
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI.setupWithNavController
-import androidx.recyclerview.widget.GridLayoutManager
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import com.halil.chatapp.R
 import com.halil.chatapp.adapter.ListAdapter
+import com.halil.chatapp.adapter.PageAdapter
 import com.halil.chatapp.data.User
+import com.halil.chatapp.databinding.ActivityMainBinding
+
 import com.halil.chatapp.ui.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var binding : ActivityMainBinding
     private val vm: MainViewModel by viewModels()
-    var database : FirebaseDatabase?=null
-    var users : ArrayList<User>?=null
-    var usersAdapter: ListAdapter?=null
+    private lateinit var tabLayout : TabLayout
+    private lateinit var viewPager : ViewPager2
+//    var database : FirebaseDatabase?=null
+//    var users : ArrayList<User>?=null
+//    var usersAdapter: ListAdapter?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.mainFragmentContainerView) as NavHostFragment
-        navController = navHostFragment.navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        setupWithNavController(bottomNavigationView, navController)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        tablayoutSet ()
 
     }
 
@@ -59,5 +56,18 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+    fun tablayoutSet (){
+        tabLayout = binding.tabLayout
+        viewPager = binding.viewPager
+        viewPager.adapter = PageAdapter(this)
+        TabLayoutMediator(tabLayout,viewPager){tab,index->
+            tab.text = when(index){
+                0-> {"Home"}
+                1->{"Settings"}
+                else->{throw NotFoundException("notFound")}
+            }
+        }.attach()
+
     }
 }
